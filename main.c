0bd0640c56b1cb639352ecf5239e02199130e512
Serban681 <serbands20@gmail.com>
2023-11-16 19:09:48 +0200

    new Commit

--- main.c
+++ main.c
@@ -27,7 +27,6 @@ DIR *dir;
 char buffer[BUFSIZ];
 struct BMPInfoHeader infoHeader;
 
-
 void verifyArgs(int argc, char **args) {
     if(argc != 2) {
         if(argc == 1) {
@@ -44,32 +43,27 @@ void verifyArgs(int argc, char **args) {
 
 void generateStatsDir(struct dirent *info) {
     struct stat fileStat;
-    fstat(info->d_name, &fileStat);
+    fstat(*info->d_name, &fileStat);
 
-    snprintf(buffer,sizeof(buffer),"nume director: %s\nidentificatorul utilizatorului: %s\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", info->d_name,info->d_name, (fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
-    write(fOut,buffer,strlen(buffer));
+    snprintf(buffer,sizeof(buffer), "nume director: %s\nidentificatorul utilizatorului: %s\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", info->d_name,info->d_name, (fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
+    write(fOut, buffer,strlen(buffer));
 }
 
 void generateStatsBmpFile(struct dirent *info) {
     struct BMPInfoHeader infoHeader;
-    ssize_t bytesRead = read(info->d_name, &infoHeader, sizeof(struct BMPInfoHeader));
     
-    if (bytesRead == -1) {
-        perror("Error reading info header");
-        close(info->d_name);
-        exit(-1);
-    }
+    read(*info->d_name, &infoHeader, sizeof(struct BMPInfoHeader));
 
     struct stat fileStat;
-    fstat(info->d_name, &fileStat);
+    fstat(*info->d_name, &fileStat);
 
-    sprintf(buffer, "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %ld\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s", info->d_name, infoHeader.height, infoHeader.width, (long long)fileStat.st_size, fileStat.st_uid, ctime(&fileStat.st_mtime), fileStat.st_nlink, (fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
+    sprintf(buffer, "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %ld\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", info->d_name, infoHeader.height, infoHeader.width, (long long)fileStat.st_size, fileStat.st_uid, ctime(&fileStat.st_mtime), fileStat.st_nlink, (fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
     write(fOut,buffer,strlen(buffer));
 }
 
 void generateStatsRegFile(struct dirent *info) {
     struct stat fileStat;
-    fstat(info->d_name, &fileStat);
+    fstat(*info->d_name, &fileStat);
 
     sprintf(buffer,"nume fisier: %s\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %ld\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", info->d_name, (long long)fileStat.st_size, fileStat.st_uid, ctime(&fileStat.st_mtime), fileStat.st_nlink, (fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
     write(fOut,buffer,strlen(buffer));
@@ -118,7 +112,6 @@ void openDir(const char *dirName,int argc, char *argv[]) {
                     generateStatsRegFile(info);
                 } 
             }
-        
             else if (info->d_type == DT_DIR) {
                 generateStatsDir(info);
                 printf("Directory: %s\n", info->d_name);
@@ -126,8 +119,7 @@ void openDir(const char *dirName,int argc, char *argv[]) {
                 continue;
             }
 
-            close(fOut);
-            closedir(dir);
+            
         }
 
     }
@@ -138,8 +130,8 @@ int main(int argc, char* args[]) {
     verifyArgs(argc, args);
 
     openDir(args[1], argc, args);
-    // generatestatisticsFile(args);
-
     
+    close(fOut);
+    closedir(dir);
     return 0;
 }
