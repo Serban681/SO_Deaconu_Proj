39f67d53d3decd1f2e1eedabf55402d662037166
Serban681 <serbands20@gmail.com>
2023-11-21 13:22:14 +0200

    Finished week 8

--- main.c
+++ main.c
@@ -43,21 +43,33 @@ DIR *outDir;
 char buffer[3000];
 struct InfoHeader infoHeader;
 int procesFiu, status;
-int pid;
+int pid[200];
 
-void writeInFile(char *buffer)
+void createFile(char *buffer, char *fileName, char *dirName)
 {
-  if(write(fOut,buffer,strlen(buffer))==-1)
-      {
-        perror("write in file output error");
-        exit(-1);
-      }
+    char outFileName[500] = "";
+    char *p = strtok(fileName, ".");
+    strcat(outFileName, p);
+    strcat(outFileName, "_statistica");
+    strcat(outFileName, ".txt");
 
+    char pathName[500] = "./";
+    strcat(pathName, dirName);
+    strcat(pathName, "/");
+    strcat(pathName, outFileName);
+
+    int fp;
+
+    fp = open(pathName, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    
+    write(fp,buffer,strlen(buffer));
+
+    close(fp);
 }
 
 void verifyArgs(int argc, char **args) {
-    if(argc != 2) {
-        if(argc == 1) {
+    if(argc != 3) {
+        if(argc == 2 || argc == 1) {
             perror("Usage ./program <director_intrare> <director_iesire>");
         }
         else {
@@ -69,80 +81,69 @@ void verifyArgs(int argc, char **args) {
     }
 }
 
-void generateStatsDir(struct dirent *info) {
+void generateStatsDir(struct dirent *info, char* dirName) {
     struct stat fileStat;
     fstat(*info->d_name, &fileStat);
 
     snprintf(buffer,sizeof(buffer), "nume director: %s\nidentificatorul utilizatorului: %s\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", info->d_name,info->d_name, (fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
     
-    write(fOut, buffer,strlen(buffer));
+    createFile(buffer, info->d_name, dirName); 
 }
 
 void generateStatsBmpFile(struct dirent *info, char* dirName) {
     struct Header header;
     struct InfoHeader infoHeader;
-
-    char fileName[1000]="";
-    char *p = strtok(info->d_name, ".");
-    strcat(fileName, p);
-    strcat(fileName, "_statistica");
-    strcat(fileName, ".txt");
-    printf("%s\n", fileName);
+    struct ColorTable colorTable;
     
     read(*info->d_name, &header, sizeof(struct Header));
     read(*info->d_name, &infoHeader, sizeof(struct InfoHeader));
+    read(*info->d_name, &colorTable, sizeof(struct ColorTable));
+
+    colorTable.red = colorTable.red * 0.299;
+    colorTable.blue = colorTable.blue * 0.114;
+    colorTable.green = colorTable.green * 0.587;
 
     struct stat fileStat;
     fstat(*info->d_name, &fileStat);
 
-    if(dir != NULL) {
-        char filepath[1000] = "";
-        snprintf(filepath, sizeof(filepath), "%s/%s", "/outDir/", fileName);
+    sprintf(buffer,
+        "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %ld\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", 
+        info->d_name, 
+        infoHeader.height, 
+        infoHeader.width, 
+        (long long)fileStat.st_size, 
+        fileStat.st_uid, 
+        ctime(&fileStat.st_mtime), 
+        fileStat.st_nlink, 
+        (fileStat.st_mode & S_IRUSR) ? "R" : "-", 
+        (fileStat.st_mode & S_IWUSR) ? "W" : "-", 
+        (fileStat.st_mode & S_IXUSR) ? "X" : "-", 
+        (fileStat.st_mode & S_IRGRP) ? "R" : "-", 
+        (fileStat.st_mode & S_IWGRP) ? "W" : "-", 
+        (fileStat.st_mode & S_IXGRP) ? "X" : "-", 
+        (fileStat.st_mode & S_IROTH) ? "R" : "-", 
+        (fileStat.st_mode & S_IWOTH) ? "W" : "-", 
+        (fileStat.st_mode & S_IXOTH) ? "X" : "-");
 
-        FILE *file = fopen(filepath, "w");
 
-        fprintf(file, "%s", "asdasdhaishdiasdhiu");
-
-        fclose(file);
-
-        // sprintf(buffer,
-        //     "nume fisier: %s\ninaltime: %d\nlungime: %d\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %ld\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", 
-        //     info->d_name, 
-        //     infoHeader.height, 
-        //     infoHeader.width, 
-        //     (long long)fileStat.st_size, 
-        //     fileStat.st_uid, 
-        //     ctime(&fileStat.st_mtime), 
-        //     fileStat.st_nlink, 
-        //     (fileStat.st_mode & S_IRUSR) ? "R" : "-", 
-        //     (fileStat.st_mode & S_IWUSR) ? "W" : "-", 
-        //     (fileStat.st_mode & S_IXUSR) ? "X" : "-", 
-        //     (fileStat.st_mode & S_IRGRP) ? "R" : "-", 
-        //     (fileStat.st_mode & S_IWGRP) ? "W" : "-", 
-        //     (fileStat.st_mode & S_IXGRP) ? "X" : "-", 
-        //     (fileStat.st_mode & S_IROTH) ? "R" : "-", 
-        //     (fileStat.st_mode & S_IWOTH) ? "W" : "-", 
-        //     (fileStat.st_mode & S_IXOTH) ? "X" : "-");
-    
-        // write(file,buffer,strlen(buffer));
-    }
+    createFile(buffer, info->d_name, dirName);    
 }
 
-void generateStatsRegFile(struct dirent *info) {
+void generateStatsRegFile(struct dirent *info, char* dirName) {
     struct stat fileStat;
     fstat(*info->d_name, &fileStat);
 
     sprintf(buffer,"nume fisier: %s\ndimensiune: %lld\nidentificatorul utilizatorului: %d\ntimpul ultimei modificari: %scontorul de legaturi: %ld\ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n", info->d_name, (long long)fileStat.st_size, fileStat.st_uid, ctime(&fileStat.st_mtime), fileStat.st_nlink, (fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
-    write(fOut,buffer,strlen(buffer));
+    createFile(buffer, info->d_name, dirName); 
 }
 
-void generateStatsLnk(struct dirent *info) {
+void generateStatsLnk(struct dirent *info, char* dirName) {
     struct stat fileStat;
     stat(info->d_name, &fileStat);
 
     sprintf(buffer, "nume legatura: %s\ndimensiune: %lld\ndimensiune fisier: %lld \n \ndrepturi de acces user: %s%s%s\ndrepturi de acces grup: %s%s%s\ndrepturi de acces altii: %s%s%s\n\n",
                      info->d_name, (long long)fileStat.st_size, (long long)fileStat.st_size,(fileStat.st_mode & S_IRUSR) ? "R" : "-", (fileStat.st_mode & S_IWUSR) ? "W" : "-", (fileStat.st_mode & S_IXUSR) ? "X" : "-", (fileStat.st_mode & S_IRGRP) ? "R" : "-", (fileStat.st_mode & S_IWGRP) ? "W" : "-", (fileStat.st_mode & S_IXGRP) ? "X" : "-", (fileStat.st_mode & S_IROTH) ? "R" : "-", (fileStat.st_mode & S_IWOTH) ? "W" : "-", (fileStat.st_mode & S_IXOTH) ? "X" : "-");
-    write(fOut,buffer,strlen(buffer));
+    createFile(buffer, info->d_name, dirName); 
 }
 
 void openDir(const char *dirName,int argc, char *argv[]) {
@@ -150,69 +151,73 @@ void openDir(const char *dirName,int argc, char *argv[]) {
     struct dirent *info;
     
     dir = opendir(dirName);
-    outDir = opendir("/outDir");
 
     if (dir == NULL) {
         perror("opendir");
         exit(EXIT_FAILURE);
     }
 
-    if (outDir == NULL) {
-        perror("opendir");
-        exit(EXIT_FAILURE);
-    }
-    
     if( (fOut = open("statistica.txt",O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) < 0 )
     {
       perror("output file error \n");
       exit(-1);
     }
 
-    while ((info = readdir(dir)) != NULL) {
+    int i = 0;
 
-        int i = 0;
+    while ((info = readdir(dir)) != NULL) {    
         struct stat fileInfo;
         stat(info->d_name, &fileInfo);
         if(S_ISREG(fileInfo.st_mode) == 0)
         {
-            if((pid = fork()) < 0){
+            int value = 0;
+            if((pid[i] = fork()) < 0){
                 perror("fork error\n");
                 exit(-1);
             }     
-            if(pid == 0){
+            if(pid[i] == 0){
                 if (info->d_type == DT_REG) {
                     const char *extension = strrchr(info->d_name, '.');
                     if (extension != NULL && strcmp(extension, ".bmp") == 0) {
                         generateStatsBmpFile(info, argv[2]);
+                        value = 10;
                     } else if(S_ISLNK(fileInfo.st_mode)){
-                        generateStatsLnk(info);
+                        generateStatsLnk(info, argv[2]);
                     }
                     else {
-                        generateStatsRegFile(info);
+                        generateStatsRegFile(info, argv[2]);
+                        value = 8;
                     } 
                 }
                 else if (info->d_type == DT_DIR) {
-                    generateStatsDir(info);
-                    printf("Directory: %s\n", info->d_name);
+                    generateStatsDir(info, argv[2]);
+                    value=5;
                 } else {
                     continue;
                 }
-                exit(i);
+
+                exit(value);
             } 
         }
 
         i++;
     }
+
+    for(int j=0; j<i; j++) {
+        int fiu = wait(&status);
+        sprintf(buffer, "Procesul cu pid-ul %d a returnat %d.\n", fiu, WEXITSTATUS(status));
+        write(fOut, buffer, sizeof(buffer));
+        sprintf(buffer, "");
+    }
 }
 
 
 int main(int argc, char* args[]) {
-    // verifyArgs(argc, args);
+    verifyArgs(argc, args);
 
     openDir(args[1], argc, args);
     
     close(fOut);
     closedir(dir);
-    closedir(outDir);
     return 0;
 }
